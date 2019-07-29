1101bcd07994997038e10389c31572335a0418a9
baodongle <baodongle@gmail.com>
2019-07-29 16:53:47 +0700

    HTTP
    - Added the necessary dependencies to use HTTP in the app.
    - Refactored `HeroService` to load heroes from a web API.
    - Extended `HeroService` to support `post()`, `put()`, and `delete()` methods.
    - Updated the components to allow adding, editing, and deleting of heroes.
    - Configured an in-memory web API.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -11,7 +11,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  hero: Hero;
+  @Input() hero: Hero;
 
   constructor(
     private route: ActivatedRoute,
@@ -27,10 +27,15 @@ export class HeroDetailComponent implements OnInit {
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
-        .subscribe(hero => this.hero = hero);
+      .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
   }
 }
